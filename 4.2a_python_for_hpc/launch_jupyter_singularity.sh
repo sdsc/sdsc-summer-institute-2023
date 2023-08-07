7f09971e545cbdd8ecddb245c7e9004cb50f0ff3
Andrea Zonca <code@andreazonca.com>
2023-08-07 14:47:16 -0700

    updated for 2023

--- 4.2a_python_for_hpc/launch_jupyter_singularity.sh
+++ 4.2a_python_for_hpc/launch_jupyter_singularity.sh
@@ -1,7 +1,6 @@
 export PATH="/cm/shared/apps/sdsc/galyleo:${PATH}"
 SIMG='/expanse/lustre/projects/sds166/zonca/dask-numba-si22.sif'
 # Summer institute account
-ACCOUNT=crl155
 # Use current folder
 NOTEBOOK_FOLDER=$(pwd -P)
 # Create link
@@ -11,5 +10,6 @@ ln -s /expanse/lustre/projects/sds166/zonca/dask_tutorial_data data
 cd ../
 # Use the user home
 # NOTEBOOK_FOLDER=${HOME}
-RESERVATION=SI2022DAY3
-galyleo.sh launch --account $ACCOUNT --reservation ${RESERVATION} --partition 'compute' --cpus-per-task 128 --time-limit 02:45:00 --jupyter 'lab' --notebook-dir "${NOTEBOOK_FOLDER}" --env-modules 'singularitypro' --bind '/expanse,/scratch' --sif "${SIMG}"
+#galyleo.sh launch --account $ACCOUNT --reservation ${RESERVATION} --partition 'compute' --cpus-per-task 128 --time-limit 02:45:00 --jupyter 'lab' --notebook-dir "${NOTEBOOK_FOLDER}" --env-modules 'singularitypro' --bind '/expanse,/scratch' --sif "${SIMG}"
+
+galyleo launch --account ${SI23_ACCOUNT} --reservation ${SI23_RES_CPU} --partition compute --qos ${SI23_QOS_CPU} --cpus 128 --memory 243 --time-limit 04:00:00 --env-modules singularitypro --       jupyter 'lab' --notebook-dir "${NOTEBOOK_FOLDER}" --sif ${SIMG} --bind '/cm,/expanse,/scratch' --quiet
