e433c5bbabdf4d1adf2fcbce8904fd376e2f22ef
HPCClass Admin <etrain95@login01.expanse.sdsc.edu>
2023-08-07 16:48:35 -0700

    fix typo

--- 4.2a_python_for_hpc/launch_jupyter_singularity.sh
+++ 4.2a_python_for_hpc/launch_jupyter_singularity.sh
@@ -12,4 +12,4 @@ cd ../
 # NOTEBOOK_FOLDER=${HOME}
 #galyleo.sh launch --account $ACCOUNT --reservation ${RESERVATION} --partition 'compute' --cpus-per-task 128 --time-limit 02:45:00 --jupyter 'lab' --notebook-dir "${NOTEBOOK_FOLDER}" --env-modules 'singularitypro' --bind '/expanse,/scratch' --sif "${SIMG}"
 
-galyleo launch --account ${SI23_ACCOUNT} --reservation ${SI23_RES_CPU} --partition compute --qos ${SI23_QOS_CPU} --cpus 128 --memory 243 --time-limit 04:00:00 --env-modules singularitypro --       jupyter 'lab' --notebook-dir "${NOTEBOOK_FOLDER}" --sif ${SIMG} --bind '/cm,/expanse,/scratch' --quiet
+galyleo launch --account ${SI23_ACCOUNT} --reservation ${SI23_RES_CPU} --partition compute --qos ${SI23_QOS_CPU} --cpus 128 --memory 243 --time-limit 04:00:00 --env-modules singularitypro --jupyter 'lab' --notebook-dir "${NOTEBOOK_FOLDER}" --sif ${SIMG} --bind '/cm,/expanse,/scratch' --quiet
