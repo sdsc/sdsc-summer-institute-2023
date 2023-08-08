d8009e04bff75f0befcb04eb1283da69039ca150
HPCClass Admin <etrain95@login02.expanse.sdsc.edu>
2023-08-08 11:36:52 -0700

    use reservation again

--- 4.2a_python_for_hpc/launch_jupyter_singularity.sh
+++ 4.2a_python_for_hpc/launch_jupyter_singularity.sh
@@ -10,6 +10,8 @@ ln -s /expanse/lustre/projects/sds166/zonca/dask_tutorial_data data
 cd ../
 # Use the user home
 # NOTEBOOK_FOLDER=${HOME}
-#galyleo.sh launch --account $ACCOUNT --reservation ${RESERVATION} --partition 'compute' --cpus-per-task 128 --time-limit 02:45:00 --jupyter 'lab' --notebook-dir "${NOTEBOOK_FOLDER}" --env-modules 'singularitypro' --bind '/expanse,/scratch' --sif "${SIMG}"
 
-galyleo launch --account ${SI23_ACCOUNT} --reservation ${SI23_RES_CPU} --partition compute --qos ${SI23_QOS_CPU} --cpus 128 --memory 243 --time-limit 04:00:00 --env-modules singularitypro --jupyter 'lab' --notebook-dir "${NOTEBOOK_FOLDER}" --sif ${SIMG} --bind '/cm,/expanse,/scratch' --quiet
+## Debug
+#galyleo launch --account ${SI23_ACCOUNT} --partition debug --cpus 128 --memory 243 --time-limit 00:30:00 --env-modules singularitypro --jupyter 'lab' --notebook-dir "${NOTEBOOK_FOLDER}" --sif ${SIMG} --bind '/cm,/expanse,/scratch' --quiet
+
+galyleo launch --reservation ${SI23_RES_CPU} --account ${SI23_ACCOUNT} --partition compute --cpus 128 --memory 243 --time-limit 04:00:00 --env-modules singularitypro --jupyter 'lab' --notebook-dir "${NOTEBOOK_FOLDER}" --sif ${SIMG} --bind '/cm,/expanse,/scratch' --quiet
