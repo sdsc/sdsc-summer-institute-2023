5379df68c829ef7eb9f56ef5fda599cdba40d641
HPCClass Admin <etrain95@login02.expanse.sdsc.edu>
2023-08-07 21:22:51 -0700

    testing on debug

--- 4.2a_python_for_hpc/dask_slurm/launch_worker.sh
+++ 4.2a_python_for_hpc/dask_slurm/launch_worker.sh
@@ -1,7 +1,7 @@
 echo "Launching dask worker"
 MEM_GB=180
-SIMG='/expanse/lustre/projects/sds166/zonca/dask-numba-si21.sif'
+SIMG='/expanse/lustre/projects/sds166/zonca/dask-numba-si23.sif'
 # memory limit is in bytes
 MEM=$(( $MEM_GB*1024**3 ))
 module load singularitypro
-singularity exec $SIMG dask-worker --scheduler-file ~/.dask_scheduler.json --memory-limit $MEM --nprocs 1
+singularity exec $SIMG dask worker --scheduler-file ~/.dask_scheduler.json --memory-limit $MEM --nworkers 1
